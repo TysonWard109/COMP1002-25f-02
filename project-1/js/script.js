cb0f467fc43f8f27001ef02e5bb12ba479f2fd9b
Tyson Ward <tysonward109@gmail.com>
2025-12-05 20:13:25 -0500

    Added a form for feedback with some JS

---
+++ project-1/js/script.js
@@ -0,0 +1,10 @@
+//Displays our comfirmation message when our feedback form in submitted
+function submitFeedback(){
+    const name = document.getElementById("name").value;
+
+    if (name === ""){
+        alert("Please enter your name before submitting.")
+    } else {
+        alert("Thank you for your feedback, " +name +"!");
+    }
+}
